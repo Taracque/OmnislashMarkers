963688603ae53ce02fde6b8f15d4e1c953004c74
Taracque <taracque@taracque.hu>
2012-02-06 11:52:18 +0100

    Added ultraxion HC markers, and warnings.
    Must be configured manually using the hot_standers array

--- OmnislashMarkers.lua
+++ OmnislashMarkers.lua
@@ -1,5 +1,43 @@
 local frame, events = CreateFrame("Frame"), {};
 local listOfPlayers = {}
+local hour_of_twilight_count = 0
+local hot_standers = {
+	1 = {
+		1 = "Dögrovás",
+		2 = "Airween",
+		3 = ""
+	},
+	2 = {
+		1 = "Zedicus",
+		2 = "Holywing",
+		3 = "Divine protection!"
+	},
+	3 = {
+		1 = "Meitra",
+		2 = "Nashmabb",
+		3 = "Pain Supression, Hand of Sacrifice"
+	},
+	4 = {
+		1 = "Dögrovás",
+		2 = "Airween",
+		3 = ""
+	},
+	5 = {
+		1 = "Zedicus",
+		2 = "Classrun",
+		3 = "Hand of Sacrifice"
+	},
+	6 = {
+		1 = "Meitra",
+		2 = "Holywing",
+		3 = "Divine Shield"
+	},
+	7 = {
+		1 = "Dögrovás",
+		2 = "Airween",
+		3 = ""
+	}
+}
 
 function events:PLAYER_ENTERING_WORLD(...)
 	print("OmnislashMarkers Loaded - requires raid assist!")
@@ -15,16 +53,17 @@ end
 
 function events:COMBAT_LOG_EVENT_UNFILTERED(self, event, ...)
  	local timestamp, combatEvent, hideCaster, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags = ...; -- Those arguments appear for all combat event variants.
+	local i
+	local icon
 
+	-- Zon'ozz HC
 	playerWithBuff = select(7, ...)
 	buffName = select(11, ...)
 
 	if buffName == "Disrupting Shadows" and event == "SPELL_AURA_APPLIED" then
 
-		local i
 		local name
 		local party
-		local icon
 		local icon1 = 1
 		local icon2 = 4
 		
@@ -68,6 +107,37 @@ function events:COMBAT_LOG_EVENT_UNFILTERED(self, event, ...)
 			SetRaidTargetIcon(destGUID, 8)
 			SendChatMessage( sourceName .. " {rt8}", "RAID" )
 		end
+	end
+	
+	-- Ultraxion HC
+	if buffName == "Last Defender of Azeroth" then
+		hour_of_twilight_count = 0
+		icon = 0
+		-- load player's guid
+		listOfPlayers = {}
+		for i=1, GetNumRaidMembers() do
+			listOfPlayers[ UnitName("raid"..i) ] = UnitGUID("raid"..i)
+		end
+	end if
+	
+	if buffName == "Hour of Twilight" then -- skull and cross
+		hour_of_twilight_count = hour_of_twilight_count + 1
+		for i=1, 2 do
+			SetRaidTargetIcon( listOfPlayers[ hot_standers[hour_of_twilight_count][i] ], 6 + i)
+		end
+		if (hot_standers[hour_of_twilight_count][3] ~= "") then
+			SendChatMessage( hot_standers[hour_of_twilight_count][3] , "RAID_WARNING" )
+		end
+	end if
+	if buffName == "Fading Light" and event == "SPELL_AURA_APPLIED" then
+		icon = icon + 1
+		SetRaidTargetIcon(destGUID, icon)
+		if (icon >= 3) then
+			icon = 0
+		end
+	end
+	if buffName == "Fading Light" and event == "SPELL_AURA_REMOVED" then
+		SetRaidTargetIcon(destGUID, 0)
 	end
 end
 
